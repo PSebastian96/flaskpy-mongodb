97560c69579f2f497a906e190ae0a6dbacf33a86
PSebastian96 <>
2023-08-27 13:10:14 +0000

    edit route

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def register():
     return render_template("register.html")
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/signin", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         # check if username exists in db
@@ -91,7 +91,7 @@ def profile(username):
     return redirect(url_for("login"))
 
 
-@app.route("/logout")
+@app.route("/signout")
 def logout():
     # remove user from session cookie
     flash("You have been logged out")
@@ -121,6 +121,19 @@ def add_task():
 
 @app.route("/edit_task/<task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "task_name": request.form.get("task_name"),
+            "task_description": request.form.get("task_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.update({"_id": ObjectId(task_id)}, submit)
+        flash("Task Successfully Updated")
+
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_task.html", task=task, categories=categories)
